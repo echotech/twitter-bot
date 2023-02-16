0d3d4a6892883e153cf46eccbec174fe32da721d
Jed Reisner <djechotech@gmail.com>
2023-02-15 22:55:28 -0700

    footer again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
+    <footer className="h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
         Powered by{" "}
         <a
@@ -22,6 +22,7 @@ export default function Footer() {
         >
           Vercel Edge Functions. 
         </a>
+        <br>
           Inspired by 
          <a
           href="https://github.com/Nutlope/twitterbio"
